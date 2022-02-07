db56c46599a86b3c8668c50ccb1ec023bc944eb7
jtm2021 <jesson_chubz@yahoo.com.ph>
2022-02-07 20:30:44 +0000

    Create get_funds_data function to get data string from the user

--- run.py
+++ run.py
@@ -12,8 +12,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('lotto_tracker')
 
-funds = SHEET.worksheet('funds')
+def get_funds_data():
+    """
+    Get funds input from the user which corresponds to each player's contribution
+    """
+    print("Please enter contributions data for each player.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 5, 10, 3, 20, 0, 8\n")
 
-data = funds.get_all_values()
+    contribution = input("Enter your data here please: ")
+    print(f"The data provided is {contribution}")
 
-print(data)
+get_funds_data()
