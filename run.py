93900fa4cd70c5064fc77b74ffbe99b3d7214de0
jtm2021 <jesson_chubz@yahoo.com.ph>
2022-02-09 16:29:14 +0000

    add validate_data function to check if values provided are valid

--- run.py
+++ run.py
@@ -19,9 +19,27 @@ def get_contributions_data():
     Get contributions from each player for the lotto draw.
     """
     print("Please enter contributions data for each player!")
-    print("Data should be eight numbers, separated by commas")
+    print("Data should be eight numbers, separated by commas.")
     print("Example: 5,10,3,2,6,5,5,2\n")
 
     data_str = input("Enter your data here: ")
+
+    contributions_data = data_str.split(",")
+    validate_data(contributions_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into integers,
+    or if the input isn't exactly 8 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"8 values required! you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.")
 
 get_contributions_data()
