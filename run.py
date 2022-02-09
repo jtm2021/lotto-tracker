ae7223816b4e7d2a1fd32b5cff4ffefc34b730b0
jtm2021 <jesson_chubz@yahoo.com.ph>
2022-02-09 20:36:37 +0000

    Add another line of message after introductory message

--- run.py
+++ run.py
@@ -12,20 +12,29 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('lotto_tracker')
 
-print("Welcome to Lotto Tracker Data Automation!\n")
+print("Welcome to Lotto Tracker Data Automation!")
+print("Please select from the menu using the corresponding number\n")
+
 
 def get_contributions_data():
     """
     Get contributions from each player for the lotto draw.
     """
-    print("Please enter contributions data for each player!")
-    print("Data should be eight numbers, separated by commas.")
-    print("Example: 5,10,3,2,6,5,5,2\n")
+    while True:
+        print("Please enter contributions data for each player!")
+        print("Data should be eight numbers, separated by commas.")
+        print("Example: 5,10,3,2,6,5,5,2\n")
+    
+        data_str = input("Enter your data here: ")
+    
+        contributions_data = data_str.split(",")
+        
+        if validate_data(contributions_data):
+            print("Data is valid!")
+            break
 
-    data_str = input("Enter your data here: ")
+    return contributions_data
 
-    contributions_data = data_str.split(",")
-    validate_data(contributions_data)
 
 def validate_data(values):
     """
@@ -35,11 +44,48 @@ def validate_data(values):
     """
     try:
         [int(value) for value in values]
-        if len(values) != 6:
+        if len(values) != 8:
             raise ValueError(
                 f"8 values required! you provided {len(values)}"
             )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.")
+    except ValueError:
+        print(f"Invalid data! Please try again.\n")
+        return False
 
-get_contributions_data()
+    return True
+
+
+def update_contributions_worksheet(data):
+    """
+    Update the contributions worksheet, adding new row with the list data provided
+    """
+    print("Updating contributions worksheet...\n")
+    contributions_worksheet = SHEET.worksheet("contributions")
+    contributions_worksheet.append_row(data)
+    print("Contributions worksheet updated succesfully.\nDo you want to make a bet for the next lotto draw?\n")
+
+    decision = input('Enter "yes" if you want to make the bet: ')
+
+    get_funds_data()
+
+
+def get_funds_data():
+    """
+    Get the last row of funds data worksheet
+    """
+    contributions = SHEET.worksheet("contributions").get_all_values()
+    contributions_last_row = contributions[-1]
+    funds = SHEET.worksheet("funds").get_all_values()
+    funds_last_row = funds[-1]
+
+    total_funds = []
+    for contributions, funds in zip(contributions_last_row, funds_last_row):
+        funds2 = int(contributions) + int(funds)
+        total_funds.append(funds2)
+
+    return total_funds
+
+data = get_contributions_data()
+contributions_data = [int(num) for num in data]
+update_contributions_worksheet(contributions_data)
+
