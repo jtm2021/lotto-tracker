1b4506508f9804f62072339d22a4b1f61daa5b07
jtm2021 <jesson_chubz@yahoo.com.ph>
2022-02-11 17:07:04 +0000

    Fix codes in get_user_choice function

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-import gspread, random
+import gspread
+import random
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -12,64 +13,79 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('lotto_tracker')
 
+
 print("Welcome to Lotto Tracker Data Automation!")
-print("Please select from the menu using the corresponding number:\n")
-print("""1 - Check Group Total Funds\n2 - Make a Bet\n3 - Input Lotto Win
-4 - Check Last Numbers\n5 - Check Member Funds\n6 - Add Member Contribution
-7 - Exit\n""")
+
 
 def get_user_choice():
     """
     Get user choice from the welcome menu.
     """
-    user_choice = int(input("Please enter your choice (from number 1-6): "))
+    print("Please select from the menu using the corresponding number:\n")
+    print("""1 - Check Group Total Funds\n2 - Make a Bet\n3 - Input Lotto Win
+4 - Check Last Numbers\n5 - Check Member Funds\n6 - Add Member Contribution
+7 - Exit\n""")
+    try:
+        user_choice = int(input("Please enter your choice (from number 1-7): "))
+        if user_choice == 1:
+            check_total_funds()
+        elif user_choice == 2:
+            get_random_number()
+        elif user_choice == 3:
+            calculate_winnings()
+        elif user_choice == 4:
+            check_last_numbers()
+        elif user_choice == 5:
+            show_member_funds()
+        elif user_choice == 6:
+            get_contributions_data()
+        elif user_choice == 7:
+            exit_program()
+    except ValueError:
+            print("\nInvalid Choice! Select a number from 1 to 7 only. Please try again.")  
+            # creates error if i make a new line of comment
+            get_user_choice()
+            # if a word is entered, it gives another error!
 
-    # make this a loop until the user inputs a correct choice
-    if user_choice == 1:
-        check_total_funds()
-    elif user_choice == 2:
-        get_random_number()
-    elif user_choice == 3:
-        calculate_winnings()
-    elif user_choice == 4:
-        check_last_numbers()
-    elif user_choice == 5:
-        pass
-    elif user_choice == 6:
-        get_contributions_data(),
-        #update_contributions_worksheet(data)
-    elif user_choice == 7:
-        exit_program()
-    else:
-        print("Invalid Choice! Select a number from 1 to 7 only. Please try again.")
-
+def show_member_funds():
+    funds = SHEET.worksheet("funds").get_all_values()
+    for i in range(len(funds[0])):
+        print(funds[0][i],":",funds[1][i])
+        # add check main menu later
 
 def calculate_winnings():
     """
     Get the value of winnings from the user and divide equally to all members
     """
-    winning_value = int(input("Please enter the amount of winnings from the bet: "))
+    winning_value = int(input("Please enter the amount of winnings: "))
     member_share = winning_value // 8
-    print(f"The group has won €{winning_value} and each member gets {member_share} each!")
+    print(f"""The group has won €{winning_value} and each member gets
+    {member_share} each!""")
+    fundsheets = SHEET.worksheet("funds")
+    fundsheets.append_row([member_share]*8)
     # print("Updating funds worksheet...")
     # add member_share to each individual and update funds sheet
+    # validate input from user for the winnings
+
 
 def check_total_funds():
     """
     Check the total amount of money of all group members
-    """    
+    """
     funds = SHEET.worksheet("funds").get_all_values()
-    funds_last_row = funds[-1]
-    print(int(funds_last_row))
-    # can't get the sum of the last rows?
+    funds_first_row = funds[1]
+    a = sum([int(i) for i in funds_first_row])
+    print(a)
+    check_main_menu()
+    #edit variable name later
 
 
 def check_member_funds():
     """
     Check each member's contributions and current total funds
     """
-    #show total amount of contributions made
-    #show current total funds in the game
+    # show total amount of contributions made
+    # show current total funds in the game
 
 
 def calculate_total_funds():
@@ -90,17 +106,33 @@ def calculate_total_funds():
     overall_balance = sum(total_funds)
     print(f"The group has {overall_balance}euros in total!")
 
- 
+
 def get_random_number():
     """
     Make a bet function using a quick pick random number generator
     """
-    bet_numbers = random.sample(list(range(1,47)), 6)
-    print(bet_numbers)
+    bet_numbers = random.sample(list(range(1, 47)), 6)
+    print(f"""Here's the quick pick numbers: {bet_numbers}
+    Feel free to copy these numbers :)""")
     numbers_worksheet = SHEET.worksheet("numbers")
     numbers_worksheet.append_row(bet_numbers)
-    #only append this values when the user confirms usage of numbers
-    #once, confirmation is made, print: numbers worksheet updated succesfully!
+    # only append this values when the user confirms usage of numbers
+    # once, confirmation is made, print: numbers worksheet updated succesfully!
+    check_main_menu()
+
+
+def check_main_menu():
+    """
+    Ask user to go back to main menu or exit
+    """
+    mx_choice = input(f"Enter yes to go back to main menu or no to exit program.")
+    if mx_choice.lower() == "yes":
+        get_user_choice()
+    elif mx_choice.lower() == "no":
+        exit_program()
+    else:
+        print(f"Invalid answer! Try again.")
+        check_main_menu()
 
 
 def check_last_numbers():
@@ -109,8 +141,11 @@ def check_last_numbers():
     """
     numbers_used = SHEET.worksheet("numbers").get_all_values()
     numbers_last_row = numbers_used[-1]
-    print(int(numbers_last_row))
-    # should return list of ints not a list of str
+    b = [int(i) for i in numbers_last_row]
+    print(b)
+    # rename variable later
+    # update sheets
+    # add check nemu function later
 
 
 def get_contributions_data():
@@ -125,12 +160,10 @@ def get_contributions_data():
         data_str = input("Enter your data here:\n")
     
         contributions_data = data_str.split(",")
-        
         if validate_data(contributions_data):
             print("Data is valid!")
+            update_contributions_worksheet(contributions_data)
             break
-
-    return contributions_data
 
 
 def validate_data(values):
@@ -152,14 +185,18 @@ def validate_data(values):
     return True
 
 
-def update_contributions_worksheet(data):
+def update_contributions_worksheet(my_list):#rename variable
     """
-    Update the contributions worksheet, adding new row with the list data provided
+    Update the contributions worksheet, adding new row with the list data
+    provided
     """
     print("Updating contributions worksheet...\n")
     contributions_worksheet = SHEET.worksheet("contributions")
-    contributions_worksheet.append_row(data)
-    print("Contributions worksheet updated succesfully.\nDo you want to make a bet for the next lotto draw?\n")
+    contributions_worksheet.append_row(my_list)#rename variable
+    print("Contributions worksheet updated succesfully\n")
+    check_main_menu()
+    
+    
 
 
 def exit_program():
@@ -168,11 +205,5 @@ def exit_program():
     """
     print("Thanks for checking in! Have a nice day! Goodbye...")
 
-    
-# data = get_contributions_data()
-# contributions_data = [int(num) for num in data]
-# update_contributions_worksheet(contributions_data)
 
 get_user_choice()
-
-
